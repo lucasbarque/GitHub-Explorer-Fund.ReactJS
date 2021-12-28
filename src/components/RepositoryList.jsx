4a5cbed5460bbd07b6da4ded754862188bba1fb1
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2021-12-28 19:16:52 -0400

    Conceitos Básicos de props

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,46 +1,21 @@
-const repositoryName = 'unform';
+import { RepositoryItem } from "./RepoitoryItem";
+
+const repository = {
+  name: 'unform',
+  description: 'forms em react',
+  link: 'https://github.com/unform/unform'
+}
 
 export function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>Lista de repositóriios</h1>
-
+      
       <ul>
-        <li>
-          <strong>{repositoryName}</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
-
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
-
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
-
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />        
       </ul>
     </section>
   );
