79c082cc1ed537a2a2715e7de74e8d73b3fcb76b
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2021-12-28 20:14:53 -0400

    Retornando em tela os repositórios

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,23 +1,24 @@
 import { RepositoryItem } from "./RepoitoryItem";
 
 import '../styles/repositories.scss';
-
-const repository = {
-  name: 'unform',
-  description: 'forms em react',
-  link: 'https://github.com/unform/unform'
-}
+import { useEffect, useState } from "react";
 
 export function RepositoryList() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+    .then(response => response.json())
+    .then(data => setRepositories(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
       
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />        
+        {repositories.map(repository => <RepositoryItem repository={repository} />)}        
       </ul>
     </section>
   );
